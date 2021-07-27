617afcc8e6eb2c7803c7f8643e033ada013d8ebc
elsiehsieh <8chingen8@gmail.com>
2021-07-27 10:06:50 +0800

    remove sequelize

--- controllers/members-controller.js
+++ controllers/members-controller.js
@@ -1,7 +1,5 @@
-const db = require('../models/db-connection');
+const dbQuery = require(`../models/${process.env.DB}-member`);
 const moment = require('moment');
-const Member = db.Member;
-const { QueryTypes } = require('sequelize');
 const message = require('../utils/message');
 module.exports = {
     getMemberByBirthday:(req,res)=>{
@@ -10,13 +8,7 @@ module.exports = {
         var date = moment(today, 'MM/DD');
         var month = date.format('M');
         var day   = date.format('D');
-     
-        const getMembers = async()=>{
-            let result = await db.sequelize.query(`SELECT * FROM members WHERE MONTH(date_of_birth) = '${month}' AND DAY(date_of_birth) = '${day}';`, { type: QueryTypes.SELECT });
-            console.log(result);
-            return result;
-        }
-        getMembers().then((members)=>{
+        dbQuery.queryMembers(month,day).then((members)=>{
             var data = [];
             if(members.length != 0){
                 members.forEach(member => {
